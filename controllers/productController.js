4ff0cd86061efd3416de3e21f421fbf1ce949c1d
AbhinavGupta <bengupta786@gmail.com>
2025-04-25 00:00:24 +0530

    change product controllers

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,68 +1,69 @@
 import prisma from "../utils/prisma.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
-const createProduct = asyncHandler(async (req,res) => {
-    const {name, price, storeId, gtin, qrCode}=req.body
-    const product = await prisma.product.create({
-      data: {
-        name,
-        price,
-        storeId,
-        gtin,
-        qrCode,
-        tags: {
-          create: tags.map(tagName => ({
-            tag: {
-              connectOrCreate: {
-                where: { name: tagName },
-                create: { name: tagName },
-              },
+const createProduct = asyncHandler(async (req, res) => {
+  const { name, price, storeId, gtin, qrCode, tags } = req.body;
+  const product = await prisma.product.create({
+    data: {
+      name,
+      price,
+      storeId,
+      gtin,
+      qrCode,
+      tags: {
+        create: tags.map((tagName) => ({
+          tag: {
+            connectOrCreate: {
+              where: { name: tagName },
+              create: { name: tagName },
             },
-          })),
-        },
+          },
+        })),
       },
-    });
-    return res.status(201).json({message:"Product created successfully"});
+    },
   });
+  return res.status(201).json({ message: "Product created successfully" });
+});
 
-  const getProductByGtin = asyncHandler(async (req,res) => {
-    const {gtin}=req.params
-    const product = await prisma.product.findUnique({
-      where: {
-        gtin,
-      },
-    });
-    return res.status(200).json({message:"Product fetched suuccessfully",data:product});
+const getProductByGtin = asyncHandler(async (req, res) => {
+  const { gtin } = req.params;
+  const product = await prisma.product.findUnique({
+    where: {
+      gtin,
+    },
+    include: { tags: { include: { tag: true } } },
   });
-  
-  const updateProduct = asyncHandler(async (req,res) => {
-    const {name, price, storeId, gtin, qrCode}=req.body
-    const {productId}=req.params
-    const updatedProduct = await prisma.product.update({
-      where: {
-        id: productId,
-      },
-      data: {
-        name,
-        price,
-        storeId,
-        gtin,
-        qrCode,
-      },
-    });
-    return res.status(200).json({message:"Product updated successfully"});
-  });
+  return res
+    .status(200)
+    .json({ message: "Product fetched suuccessfully", data: product });
+});
 
-  const deleteProduct = asyncHandler(async (req,res) => {
-    const {productId}=req.params
-    const deletedProduct = await prisma.product.delete({
-      where: {
-        id: productId,
-      },
-    });
-    return res.status(204);
+const updateProduct = asyncHandler(async (req, res) => {
+  const { name, price, storeId, gtin, qrCode } = req.body;
+  const { productId } = req.params;
+  const updatedProduct = await prisma.product.update({
+    where: {
+      id: productId,
+    },
+    data: {
+      name,
+      price,
+      storeId,
+      gtin,
+      qrCode,
+    },
   });
-  
-  
+  return res.status(200).json({ message: "Product updated successfully" });
+});
 
-  export {createProduct,getProductByGtin,updateProduct,deleteProduct}
+const deleteProduct = asyncHandler(async (req, res) => {
+  const { productId } = req.params;
+  const deletedProduct = await prisma.product.delete({
+    where: {
+      id: productId,
+    },
+  });
+  return res.status(204);
+});
+
+export { createProduct, getProductByGtin, updateProduct, deleteProduct };
